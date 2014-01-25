a79c912c8bb0b82ac32bc3f71ecb2eadf0f7c552
mrp1q7z <mrp1q7z@gmail.com>
2014-01-25 19:14:47 +0900

    first commit

---
+++ src/appinventor/ai_mrp1q7z/SilentPrayer/UsageScreen.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"83","Source":"Form","Properties":{"$Name":"UsageScreen","$Type":"Form","$Version":"11","Uuid":"0","Scrollable":"False","Title":"\u4f7f\u3044\u65b9","$Components":[{"$Name":"WebUsage","$Type":"WebViewer","$Version":"3","Uuid":"-368790439"}]}}
+|#
